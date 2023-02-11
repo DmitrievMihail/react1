b1ad8d535b717bafc034f192db6463910ac7b92f
Mihail Dmitriev <mmxdmv@mail.ru>
2023-02-12 01:45:07 +0400

    :

--- src/componets/ReviewFinder.tsx
+++ src/componets/ReviewFinder.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from './../styles/ReviewFinder.module.css';
 
 const ReviewFinder = (props: JSX.IntrinsicAttributes & React.ClassAttributes<HTMLDivElement> & React.HTMLAttributes<HTMLDivElement>) => {
+    const [login, setLogin] = useState('123');
+    const changeLogin = (login: string) => {
+        setLogin(login);
+    };
+
     return (
         <div {...props} className={classes.ReviewFinder}>
             <ul className={classes.ReviewFinderList}>
                 <li>
-                    Логин: <input placeholder='Логин текущего юзера' />
+                    Логин: <input placeholder='Логин текущего юзера' value={login} onChange={(e) => changeLogin(e.target.value)} />
                 </li>
                 <li>
                     Репозиторий: <input placeholder='Репозиторий (для поиска ревьюера)' />
