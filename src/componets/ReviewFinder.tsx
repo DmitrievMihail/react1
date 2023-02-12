d67188dbe80b8c94dd67795a73b68602a6525fe5
Mihail Dmitriev <mmxdmv@mail.ru>
2023-02-12 11:23:12 +0400

    :

--- src/componets/ReviewFinder.tsx
+++ src/componets/ReviewFinder.tsx
@@ -1,23 +1,58 @@
 import React, {useState} from 'react';
+// eslint-disable-next-line
 import classes from './../styles/ReviewFinder.module.css';
+// import { stringify } from 'querystring';
 
 const ReviewFinder = (props: JSX.IntrinsicAttributes & React.ClassAttributes<HTMLDivElement> & React.HTMLAttributes<HTMLDivElement>) => {
-    const [login, setLogin] = useState('123');
-    const changeLogin = (login: string) => {
-        setLogin(login);
+
+    // eslint-disable-next-line
+    type ValidField = | 'login' | 'repo' | 'black';
+    // eslint-disable-next-line
+    type reviewDataType = {login: string, repo: string, black: string};
+    const reviewData: reviewDataType = {login: '', repo: '', black: ''};
+    if (localStorage.hasOwnProperty('ReviewFinderData') && localStorage.getItem('ReviewFinderData')) {
+        const reviewDataTemp = JSON.parse(localStorage.getItem('ReviewFinderData') || '{}');
+        for (const key in reviewData) {
+            if (reviewDataTemp[key] && (typeof reviewDataTemp[key] === 'string')) {
+                // @ts-expect-error: тупой TS не понимает свой же тип ключа
+                reviewData[key] = reviewDataTemp[key];
+            }
+        }
+    }
+
+    const [input, setInput] = useState(reviewData);
+    const changeInput = (field: ValidField, value: string) => {
+        setInput({...input, [field]: value});
+        localStorage.setItem('ReviewFinderData', JSON.stringify(input));
+        console.log(input);
     };
 
     return (
         <div {...props} className={classes.ReviewFinder}>
             <ul className={classes.ReviewFinderList}>
                 <li>
-                    Логин: <input placeholder='Логин текущего юзера' value={login} onChange={(e) => changeLogin(e.target.value)} />
+                    Логин: <input
+                        type='text'
+                        placeholder='Логин текущего юзера'
+                        value={input.login}
+                        onChange={(e) => changeInput('login', e.target.value)}
+                    />
                 </li>
                 <li>
-                    Репозиторий: <input placeholder='Репозиторий (для поиска ревьюера)' />
+                    Репозиторий: <input
+                        type='text'
+                        placeholder='Репозиторий (для поиска ревьюера)'
+                        value={input.repo}
+                        onChange={(e) => changeInput('repo', e.target.value)}
+                    />
                 </li>
                 <li>
-                    Чёрный список: <input placeholder='Логины, которые не должны быть ревьюерами' />
+                    Чёрный список: <input
+                        type='text'
+                        placeholder='Логины, которые не должны быть ревьюерами'
+                        value={input.black}
+                        onChange={(e) => changeInput('black', e.target.value)}
+                    />
                 </li>
             </ul>
             {props.children}
