b6a73b9d9b75b0fcf40ba383757cf2392825b261
Mihail Dmitriev <mmxdmv@mail.ru>
2023-02-12 15:47:20 +0400

    :

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,71 @@ import React, { useState, useRef, useMemo } from 'react';
 import './App.css';
 import ReviewFinder from './componets/ReviewFinder';
 
+
+// Hook
+function useLocalStorage<T>(key: string, initialValue: T) {
+  // State to store our value
+  // Pass initial state function to useState so logic is only executed once
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    if (typeof window === "undefined") {
+      return initialValue;
+    }
+    try {
+      // Get from local storage by key
+      const item = window.localStorage.getItem(key);
+      // Parse stored json or if none return initialValue
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      // If error also return initialValue
+      console.log(error);
+      return initialValue;
+    }
+  });
+  // Return a wrapped version of useState's setter function that ...
+  // ... persists the new value to localStorage.
+  const setValue = (value: T | ((val: T) => T)) => {
+    try {
+      // Allow value to be a function so we have same API as useState
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      // Save state
+      setStoredValue(valueToStore);
+      // Save to local storage
+      if (typeof window !== "undefined") {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
+    } catch (error) {
+      // A more advanced implementation would handle the error case
+      console.log(error);
+    }
+  };
+  return [storedValue, setValue] as const;
+}
+
+
 function App() {
-  const [settingsVisibility, setSettingsVisibility] = useState(false);
+  let settingsVisibilityTemp = false;
+  if (localStorage.hasOwnProperty('ReviewFinderData')) {
+    const reviewDataTemp = JSON.parse(localStorage.getItem('ReviewFinderData') || '{settingsVisibility: false}');
+    if (reviewDataTemp.settingsVisibility) {
+      settingsVisibilityTemp = true;
+    }
+    console.log('Отображение: ', settingsVisibilityTemp);
+  }
+  const [settingsVisibility, setSettingsVisibility] = useState(settingsVisibilityTemp);
   const toggleSettings = () => {
     setSettingsVisibility(!settingsVisibility);
+    let reviewData = {settingsVisibility: false};
+    if (localStorage.hasOwnProperty('ReviewFinderData')) {
+      reviewData = JSON.parse(localStorage.getItem('ReviewFinderData') || '{settingsVisibility: false}');
+    }
+    reviewData.settingsVisibility = !settingsVisibility;
+    localStorage.setItem('ReviewFinderData', JSON.stringify(reviewData));
+    console.log(reviewData);
+  };
+
+  const getReviewer = () => {
+    console.log('Получим ревьювера');
   };
   return (
     <div className="App">
@@ -14,9 +75,10 @@ function App() {
       <button className="settingsButton" onClick={toggleSettings}>{settingsVisibility ? 'Скрыть' : 'Показать'} настройки</button>
 
       {settingsVisibility
-        ? <ReviewFinder />
+        ? <ReviewFinder visible={settingsVisibility ? 'true' : 'false'} />
         : <span></span>
       }
+      <button className="findButton" onClick={getReviewer}>Найти ревьюера</button>
     </div>
   );
 }
--- src/componets/ReviewFinder.tsx
+++ src/componets/ReviewFinder.tsx
@@ -1,30 +1,80 @@
-import React, {useState} from 'react';
+import React, {useState } from 'react';
 // eslint-disable-next-line
 import classes from './../styles/ReviewFinder.module.css';
 // import { stringify } from 'querystring';
 
-const ReviewFinder = (props: JSX.IntrinsicAttributes & React.ClassAttributes<HTMLDivElement> & React.HTMLAttributes<HTMLDivElement>) => {
+// Hook
+function useLocalStorage<T>(key: string, initialValue: T) {
+    // State to store our value
+    // Pass initial state function to useState so logic is only executed once
+    const [storedValue, setStoredValue] = useState<T>(() => {
+      if (typeof window === "undefined") {
+        return initialValue;
+      }
+      try {
+        // Get from local storage by key
+        const item = window.localStorage.getItem(key);
+        // Parse stored json or if none return initialValue
+        return item ? JSON.parse(item) : initialValue;
+      } catch (error) {
+        // If error also return initialValue
+        console.log(error);
+        return initialValue;
+      }
+    });
+
+
+const ReviewFinder = (props: {visible: 'true' | 'false'}) => {
 
     // eslint-disable-next-line
-    type ValidField = | 'login' | 'repo' | 'black';
+    type ValidField = | 'login' | 'repo' | 'black' | 'settingsVisibility';
     // eslint-disable-next-line
-    type reviewDataType = {login: string, repo: string, black: string};
-    const reviewData: reviewDataType = {login: '', repo: '', black: ''};
-    if (localStorage.hasOwnProperty('ReviewFinderData') && localStorage.getItem('ReviewFinderData')) {
-        const reviewDataTemp = JSON.parse(localStorage.getItem('ReviewFinderData') || '{}');
-        for (const key in reviewData) {
-            if (reviewDataTemp[key] && (typeof reviewDataTemp[key] === 'string')) {
-                // @ts-expect-error: тупой TS не понимает свой же тип ключа
-                reviewData[key] = reviewDataTemp[key];
+    type reviewDataType = {login: string, repo: string, black: string, settingsVisibility: boolean};
+/*
+    const reviewData: reviewDataType;
+    if(!Object.keys(reviewData).length){
+        const reviewData: reviewDataType = {login: '', repo: '', black: '', settingsVisibility: false};
+        if (localStorage.hasOwnProperty('ReviewFinderData')) {
+            const reviewDataTemp = JSON.parse(localStorage.getItem('ReviewFinderData') || '{}');
+            console.log('Из стораджа:', reviewDataTemp);
+            for (const key in reviewData) {
+                if (reviewDataTemp[key] && (typeof reviewDataTemp[key] === 'string' || typeof reviewDataTemp[key] === 'boolean')) {
+                    // @ts-expect-error: тупой TS не понимает свой же тип ключа
+                    reviewData[key] = reviewDataTemp[key];
+                    // console.log('*', key, reviewDataTemp[key]);
+                }
             }
+            console.log('Получили в реальности:', reviewData);
         }
     }
+    */
 
-    const [input, setInput] = useState(reviewData);
+    const [input, setInput] = useState({login: '', repo: '', black: '', settingsVisibility: false});
+    const [name, setName] = useLocalStorage<string>("name", "Bob");
+    /*
+    if (!Object.keys(input).length) {
+        const reviewData: reviewDataType = {login: '', repo: '', black: '', settingsVisibility: false};
+        if (localStorage.hasOwnProperty('ReviewFinderData')) {
+            const reviewDataTemp = JSON.parse(localStorage.getItem('ReviewFinderData') || '{}');
+            console.log('Из стораджа:', reviewDataTemp);
+            for (const key in reviewData) {
+                if (reviewDataTemp[key] && (typeof reviewDataTemp[key] === 'string' || typeof reviewDataTemp[key] === 'boolean')) {
+                    // @ts-expect-error: тупой TS не понимает свой же тип ключа
+                    reviewData[key] = reviewDataTemp[key];
+                    // console.log('*', key, reviewDataTemp[key]);
+                }
+            }
+            console.log('Получили в реальности:', reviewData);
+        }
+        setInput(reviewData);
+        input = reviewData;
+    }
+    */
     const changeInput = (field: ValidField, value: string) => {
-        setInput({...input, [field]: value});
-        localStorage.setItem('ReviewFinderData', JSON.stringify(input));
-        console.log(input);
+        const inputData = {...input, ...{[field]: value, settingsVisibility: props.visible === 'true'}};
+        setInput(inputData);
+        localStorage.setItem('ReviewFinderData', JSON.stringify(inputData));
+        console.log('Пишем в сторадж', inputData);
     };
 
     return (
@@ -55,7 +105,6 @@ const ReviewFinder = (props: JSX.IntrinsicAttributes & React.ClassAttributes<HTM
                     />
                 </li>
             </ul>
-            {props.children}
         </div>
     );
 };
